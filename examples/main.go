06f55f67be6366168de329d4f26b33d4a5a6098f
xiongChuanhong <18881934641@163.com>
2021-07-19 13:38:28 +0800

    feat: fix param

--- examples/main.go
+++ examples/main.go
@@ -10,10 +10,8 @@ func main() {
 
 	for {
 		cup := cmetric.CurrentCpuUsage()
-		if cup >= 0.5 {
-			fmt.Println("cpu ", cup)
-		}
-		time.Sleep(100 * time.Millisecond)
+		fmt.Println("cpu ", cup)
+		time.Sleep(2000 * time.Millisecond)
 	}
 
 }
